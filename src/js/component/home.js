dd3aea336f458381249f804533fe5755731246bb
byronvillalobos <byronvillalobos@hotmail.com>
2021-03-10 23:25:14 +0000

    comments

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,7 +1,7 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import "../../styles/index.scss";
 
 //create your first component
 export function Home() {
@@ -11,38 +11,50 @@ export function Home() {
 		if (inputValue === "") alert("No tasks, add a task");
 	};
 	let countItems = list.length;
+	const deletetask = index => {
+		let newList = list;
+		newList.splice(index, 1);
+		console.log(newList);
+		setlist([...newList]);
+	};
 
 	return (
-		<div className="container mt-5 text-center">
-			<div>
-				<div>
-					{" "}
-					<h1>To Do List </h1>{" "}
+		<div className="container text-center mt-5 d-flex justify-content-center">
+			<div className="card col-6">
+				<h5 className="card-title">To Do List </h5>
+				<div className="input-group mb-4 col-12 justify-content-center">
+					<input
+						id="myInput"
+						type="text"
+						className="form-control"
+						label="Task"
+						placeholder="Type your task"
+						onFocus={validateTask}
+						onChange={e => {
+							setInputValue(e.target.value);
+						}}
+						value={inputValue}
+						onKeyPress={e => {
+							if (e.key === "Enter") {
+								setlist(list.concat(inputValue));
+								setInputValue("");
+							}
+						}}
+					/>
 				</div>
 
-				<input
-					id="myInput"
-					type="text"
-					label="Task"
-					placeholder="input a value"
-					onFocus={validateTask}
-					onChange={e => {
-						setInputValue(e.target.value);
-					}}
-					value={inputValue}
-					onKeyPress={e => {
-						if (e.key === "Enter") {
-							setlist(list.concat(inputValue));
-							setInputValue("");
-						}
-					}}
-				/>
-				<div className="border border-dark">
-					<dl>
-						{list.map(name => (
-							<dt key={name}> {name} </dt>
-						))}
-					</dl>
+				<div className="card-body">
+					{list.map((name, index) => (
+						<li key={name} className="list-group-item">
+							{" "}
+							<input
+								type="checkbox"
+								className="form-check-input item float-left"
+								onClick={() => deletetask(index)}
+							/>
+							{name}{" "}
+						</li>
+					))}
 				</div>
 
 				<div>
@@ -53,18 +65,13 @@ export function Home() {
 							setlist([]);
 						}}
 						value="Reset"></input>
+					{/* <input
+						className="btn btn-primary"
+						type="Submit"
+						value="Submit"></input> */}
 					<p>Total of tasks: {countItems}</p>
 				</div>
 			</div>
-			{/* <input
-				className="btn btn-primary"
-				type="Submit"
-				onClick={e => {
-					setlist("");
-				}}
-				value="Submit"></input> */}
-
-			{/* for individual deletion https://stackoverflow.com/questions/5767325/how-can-i-remove-a-specific-item-from-an-array */}
 		</div>
 	);
 }
